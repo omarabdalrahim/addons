6518229bd0aaa6b159fd3990807a6c5b46cae2c4
OmarAbdalrahim <omar44445@yahoo.com>
2025-07-19 14:18:57 +0300

    v2

--- mentors_bonus_penalty/models/contract_changes.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields, _, api
-from odoo.osv import expression
-
-
-class EmployeeNumber(models.Model):
-    _inherit = 'hr.contract'
-
-    fixed_bonus_ids = fields.One2many('fixed.bonus', 'contract_id', 'Fixed Bonus')
